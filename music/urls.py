e7a9ab749c7b2bc92d8144c5a624c6bdceac91b3
mohammad <mohammadjavadsh801@gmail.com>
2025-04-08 18:09:14 +0330

    responsived home page

--- music/urls.py
+++ music/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path 
 from . import views 
 
+app_name = "music"
+
 urlpatterns = [
     path("",views.home_page,name="home"),
+    path("music/<str:slug>/",views.detail_page,name="detail"),
+    path("ERROR404/",views.page_404,name="404"),
 ]
--- music/views.py
+++ music/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Audio
 # Create your views here.
 
 def home_page(request):
-    audios = Audio.objects.all()
+    audios = Audio.objects.all()[:5]
     return render(request,"music/home.html",{"audios":audios})
+
+def detail_page(request,slug):
+    audio = Audio.objects.filter(slug = slug).first()
+    
+    if audio:
+        
+        return render(request, "music/detail.html", {"audio":audio})
+    
+    else:
+        return redirect("music:404")
+    
+
+def page_404(request):
+    return render(request,"music/404.html",{})
+    
